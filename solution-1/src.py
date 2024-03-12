18cf6eea12d231e88727e5906a138ba1e598c276
mendolmark <mendolmark1998@gmail.com>
2024-03-12 14:27:36 +0100

    firsttry

--- solution-1/src.py
+++ solution-1/src.py
@@ -1,26 +1,32 @@
 import pandas as pd
+from scipy.spatial import KDTree
 
 if __name__ == "__main__":
-
     df = pd.read_csv("input.csv")
     query_df = pd.read_csv("query.csv")
 
     damage_types = df["dmg_type"].unique()
     out = []
+
+    # saving indicies from the original df
+    trees = {dt: (KDTree(df.loc[df["dmg_type"] == dt, ["x", "y"]]), df.loc[df["dmg_type"] == dt].index) for dt in damage_types}
+
     for idx, row in query_df.iterrows():
         out_row = {}
         for dt in damage_types:
-            sub_df = df.loc[
-                (df["dmg_type"] == dt)
-                & (df["dmg"] >= row["dmg_min"])
-                & (df["dmg"] <= row["dmg_max"]),
-                ["x", "y", "dmg"],
-            ]
-            if sub_df.empty:
-                out_row[dt] = 0
+            tree, indices = trees[dt]
+            sub_df = df.loc[df["dmg_type"] == dt]
+
+            # finding nearest point
+            _, nearest_index = tree.query([row["x"], row["y"]], k=1)
+            original_index = indices[nearest_index] 
+
+            # check if in range
+            closest_point = sub_df.loc[original_index]
+            if closest_point["dmg"] >= row["dmg_min"] and closest_point["dmg"] <= row["dmg_max"]:
+                out_row[dt] = closest_point["dmg"]
             else:
-                diffs = ((sub_df[["x", "y"]] - row) ** 2).sum(axis=1)
-                out_row[dt] = sub_df.iloc[diffs.argmin(), 2]
+                out_row[dt] = 0
 
         out.append(out_row)
 
